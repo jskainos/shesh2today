ae2742b196deb1e624061f875f97a0eec34f7b86
Thomas McDonnell <t.mcdonnell@kainos.com>
2018-06-28 15:09:38 +0100

    dding script to generate sales table

---
+++ scripts/salesTableCreate.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `sales` (
+  `employee_number` int(11) DEFAULT NULL,
+  `commissionRate` decimal(3,2) DEFAULT NULL,
+  KEY `empNo_idx` (`employee_number`),
+  CONSTRAINT `empNo` FOREIGN KEY (`employee_number`) REFERENCES `employee` (`employee_number`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
