5349fc6606fcab4d4b30a900b33f485bc5852dcf
Thomas McDonnell <t.mcdonnell@kainos.com>
2018-06-28 10:45:32 +0100

    Adding the employees properties file for the DB connection

--- src/model/EmoloyeeQueries.java
+++ src/model/EmoloyeeQueries.java
@@ -3,12 +3,8 @@ package model;
 public class EmoloyeeQueries {
 
 	public EmoloyeeQueries() {
-		// TODO Auto-generated constructor stub
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
