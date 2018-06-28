4aed3b61fe13953e039c824492383bf27710cbe2
Thomas McDonnell <t.mcdonnell@kainos.com>
2018-06-28 12:34:32 +0100

    Updating employee model file with all relevant fields

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -7,11 +7,16 @@ public class Employee {
 	private String name;
 	private String niNumber;
 	private String department;
-	private LocalDate dob;
+	private String dob;
 	
-	public Employee(String name) {
+	public Employee(String name, String niNumber, String department, String dob) {
 		this.name = name;
+		this.niNumber = niNumber;
+		this.department = department;
+		this.dob = dob;
 	}
+	
+	
 	
 	public void setName(String name) {
 		this.name = name;
@@ -23,6 +28,34 @@ public class Employee {
 	
 	public void postToDB() {
 		EmployeeQueries.insertEmployees(this);
+	}
+
+	public int getEmployeeNum() {
+		return employeeNum;
+	}
+
+	public String getNiNumber() {
+		return niNumber;
+	}
+
+	public void setNiNumber(String niNumber) {
+		this.niNumber = niNumber;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getDob() {
+		return dob;
+	}
+
+	public void setDob(String dob) {
+		this.dob = dob;
 	}
 	
 }
